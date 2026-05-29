4d19f45af5493ed90c3c5abadbce1e90dcdfd556
shamyratpashiyev <shamyratpashiyev96@gmail.com>
2026-05-29 14:04:03 +0500

    Fixed DbContext logic mistake

--- src/Data/DbContext.php
+++ src/Data/DbContext.php
@@ -11,9 +11,13 @@ class DbContext {
         if (DbContext::$context != null) {
             return DbContext::$context;
         }
-        $connectionString = $_ENV["DB_CONNECTION_STRING"];
         $userName = $_ENV["DB_USERNAME"];
         $password = $_ENV["DB_PASSWORD"];
+        $dbName = $_ENV["DB_NAME"];
+        $dbProvider = $_ENV["DB_PROVIDER"];
+        $dbHost = $_ENV["DB_HOST"];
+        $dbCharset = $_ENV["DB_CHARSET"];
+        $connectionString = "{$dbProvider}:host={$dbHost};dbname={$dbName};charset={$dbCharset}";
         $options = [
             PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
